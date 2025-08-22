b3f38d4d30fbf7fe8458f089f8c570587638ab08
codingisfuk <wwb8524@naver.com>
2025-08-22 16:21:42 +0900

    spring boot

--- src/main/java/com/example/k5_iot_springboot/controller/F_BoardController.java
+++ src/main/java/com/example/k5_iot_springboot/controller/F_BoardController.java
@@ -9,6 +9,9 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(ApiMappingPattern.Boards.ROOT) // "/api/v1/Boards"
+@RequestMapping(ApiMappingPattern.Boards.ROOT)
 @RequiredArgsConstructor
 @Validated
 public class F_BoardController {
@@ -48,8 +51,15 @@ public class F_BoardController {
             @RequestParam(defaultValue = "10") @Min(1) @Max(100) int size,
             // sort: 여러 개 허용 - EX) sort=createAt,desc&sort=title,asc
             @RequestParam(required = false) String[] sort
+//            @PageableDefault(
+//                    page = 0,           // 기본 페이지 번호
+//                    size = 10,          // 기본 페이지 크기
+//                    sort = "createdAt", // 기본 정렬 컬럼
+//                    direction = Sort.Direction.DESC // 기본 정렬 방향
+//            ) Pageable pageable
     ) {
         ResponseDto<BoardResponseDto.PageResponse> response = boardService.getBoardsPage(page, size, sort);
+//        ResponseDto<BoardResponseDto.PageResponse> response = boardService.getBoardsPage(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
